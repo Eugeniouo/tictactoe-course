640f135e4f5400e518dde7b35ba9fbd026f5421a
Eugene Karelskiy <ekarelskij1@gmail.com>
2026-05-07 22:18:32 +0300

    [#22]chore: replace some old files and modules

--- src/player/move_ordering.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "move_generator.hpp"
-#include "search_board.hpp"
-
-namespace ttt::my_player
-{
-    void order_candidate_moves(SearchBoard &board, MoveList &moves);
-}
--- src/player/my_observer.hpp
+++ src/player/my_observer.hpp
@@ -2,16 +2,18 @@
 
 #include "core/game.hpp"
 
-namespace ttt::my_player {
+namespace ttt::my_player
+{
 
-using game::Event;
-using game::IObserver;
-using game::State;
+  using game::Event;
+  using game::IObserver;
+  using game::State;
 
-class ConsoleWriter : public IObserver {
-public:
-  static void print_game_state(const State &state);
-  void handle_event(const State &game, const Event &event) override;
+  class ConsoleWriter : public IObserver
+  {
+  public:
+    static void print_game_state(const State &state);
+    void handle_event(const State &game, const Event &event) override;
+  };
+
 };
-
-}; // namespace ttt::my_player
